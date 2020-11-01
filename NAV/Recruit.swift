168f7cbc0225173bbb1e522e9310588557c42248
xsurgefl7 <sergiozy6693@gmail.com>
2020-11-01 16:23:30 -0600

    Initial Commit

---
+++ NAV/Recruit.swift
@@ -0,0 +1,29 @@
+//
+//  Recruitment.swift
+//  NAV
+//
+//  Created by Sergio Salazar on 11/1/20.
+//
+
+import UIKit
+
+class Recruit: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
